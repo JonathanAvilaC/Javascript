f27a3a4b1959712a425239b1c13c27dcf95e4786
Jonas <jonathan.avila.cruz@hotmail.com>
2022-08-28 19:21:07 -0400

    Actividad 1 fix

--- script/actividad1.js
+++ script/actividad1.js
@@ -11,6 +11,7 @@ console.log("")
 let cond = window.confirm("Debes abrir la consola con [F12] para ver las instrucciones y jugar. Si no las ves, presiona cancelar.");
 
 if (cond) {
+    alert("Se recomienda abrir la consola lo suficiente para una correcta visualización.");
     let name = prompt("Ingresa tu nombre");
 
     while (name == "" || name == null) {
@@ -35,34 +36,38 @@ if (cond) {
 
     while (game) {
         console.clear();
-        if(p1 != undefined){
+        if (p1 != undefined) {
             console.log("Comments:")
-            console.log(name + " ha sacado " + playerRandom1)
-            
-            switch (true){
-                case(playerRandom1 > 9):
-                    console.log(m1);
-                    break;
-                case (playerRandom1 >4):
-                    console.log(m2);
-                    break;
-                case (playerRandom1 > 2):
-                    console.log(m3);
-                    break;
-                case (playerRandom1 > 0):
-                    console.log(m4);
-                    break;
-                default:
-                    console.log(m5);
-                    break;
-            } 
+            if (!p1) {
+                console.log(name + " ha pasado su turno")
+            } else {
+                console.log(name + " ha sacado " + playerRandom1)
+                switch (true) {
+                    case (playerRandom1 > 9):
+                        console.log(m1);
+                        break;
+                    case (playerRandom1 > 4):
+                        console.log(m2);
+                        break;
+                    case (playerRandom1 > 2):
+                        console.log(m3);
+                        break;
+                    case (playerRandom1 > 0):
+                        console.log(m4);
+                        break;
+                    default:
+                        console.log(m5);
+                        break;
+                }
+                
+            }
             console.log("")
-            console.log("Robocito ha sacado " + playerRandom2)
-            switch (true){
-                case(playerRandom2 > 9):
+                console.log("Robocito ha sacado " + playerRandom2)
+            switch (true) {
+                case (playerRandom2 > 9):
                     console.log(m1);
                     break;
-                case (playerRandom2 >4):
+                case (playerRandom2 > 4):
                     console.log(m2);
                     break;
                 case (playerRandom2 > 2):
@@ -74,31 +79,31 @@ if (cond) {
                 default:
                     console.log(m5);
                     break;
-            } 
-    
+            }
+
             console.log("_".repeat(60))
             console.log(`${name} (${player1}) ` + character.repeat(player1))
             console.log("_".repeat(60))
-    
+
             console.log("_".repeat(60))
             console.log(`Robocito (${player2}) ` + character.repeat(player2))
             console.log("_".repeat(60))
         }
-        
 
-        console.log("                                                                                                              __\\_/__")
-        console.log("                                                                                                              | @ @ |")
-        console.log("                                                                                                              | +++ | ")
-        console.log("                                                                                                        0=======   =======0 ")
-        console.log("                                                                                                                || ")
-        console.log("                                                                                                                || ")
-        console.log("                                                                                                               //\\\\")
-        console.log("                                                                                                              //  \\\\")
-        console.log("                                                                                                             //    \\\\")
-        console.log("                                                                                                           <==      ==>")
+
+        console.log("        __\\_/__")
+        console.log("        | @ @ |")
+        console.log("        | +++ | ")
+        console.log("  0=======   =======0 ")
+        console.log("          || ")
+        console.log("          || ")
+        console.log("         //\\\\")
+        console.log("        //  \\\\")
+        console.log("       //    \\\\")
+        console.log("     <==      ==>")
 
         p1 = window.confirm("Es tu turno ¿Deseas lanzar? Si presionas cancelar tu turno se saltará.")
-        
+
         playerRandom1 = Math.floor(Math.random() * 10);
         playerRandom2 = Math.floor(Math.random() * 10);
 
@@ -108,7 +113,7 @@ if (cond) {
 
         player1 = player1 - playerRandom2;
 
-        if (player2 <= 0 && player1 > 0){
+        if (player2 <= 0 && player1 > 0) {
             player2 = 0;
             game = false;
             console.clear();
@@ -119,19 +124,19 @@ if (cond) {
             console.log(`Robocito (${player2}) ` + character.repeat(player2))
             console.log("_".repeat(60))
 
-            console.log("                                                                                                              ~~    ~~")
-            console.log("                                                                                                              __\\_/__")
-            console.log("                                                                                                              | X X |")
-            console.log("                                                                                                              | ___ | ")
-            console.log("                                                                                                        0=======   =======0 ")
-            console.log("                                                                                                                || ")
-            console.log("                                                                                                                || ")
-            console.log("                                                                                                               //\\\\")
-            console.log("                                                                                                              //  \\\\")
-            console.log("                                                                                                             //    \\\\")
-            console.log("                                                                                                           <==      ==>")
+            console.log("        ~~    ~~")
+            console.log("        __\\_/__")
+            console.log("        | X X |")
+            console.log("        | ___ | ")
+            console.log("  0=======   =======0 ")
+            console.log("          || ")
+            console.log("          || ")
+            console.log("         //\\\\")
+            console.log("        //  \\\\")
+            console.log("       //    \\\\")
+            console.log("     <==      ==>")
             alert("Derrotaste a Robocito ¡Felicitaciones!")
-        }else if (player1 <= 0 && player2 > 0) {
+        } else if (player1 <= 0 && player2 > 0) {
             player1 = 0;
             game = false;
             console.clear();
@@ -142,14 +147,14 @@ if (cond) {
             console.log(`Robocito (${player2 < 0 ? 0 : player2}) ` + character.repeat(player2 < 0 ? 0 : player2))
             console.log("_".repeat(60))
 
-            console.log("                                                                                                              ||")
-            console.log("                                                                                                              ||")
-            console.log("                                                                                                         ====    ====")
-            console.log("                                                                                                              ||")
-            console.log("                                                                                                              || ")
-            console.log("                                                                                                              || ")
-            console.log("                                                                                                         =============")
-            console.log("                                                                                                         =============")
+            console.log("        ||")
+            console.log("        ||")
+            console.log("   ====    ====")
+            console.log("        ||")
+            console.log("        || ")
+            console.log("        || ")
+            console.log("   =============")
+            console.log("   =============")
 
             alert("Que lastima, no pudiste vencer a Robocito.")
 
@@ -166,17 +171,17 @@ if (cond) {
             console.log("_".repeat(60))
 
 
-            console.log("                                                    ~~    ~~")
-            console.log("                                                    __\\_/__")
-            console.log("                                                    | X X |")
-            console.log("                                                    | ___ |                        ||")
-            console.log("                                              0=======   =======0                  ||")
-            console.log("                                                       ||                     ====    ====")
-            console.log("                                                       ||                          ||")
-            console.log("                                                      //\\\\                         ||")
-            console.log("                                                     //  \\\\                        || ")
-            console.log("                                                    //    \\\\                  =============")
-            console.log("                                                  <==      ==>                =============")
+            console.log("           ~~    ~~")
+            console.log("           __\\_/__")
+            console.log("           | X X |")
+            console.log("           | ___ |                        ||")
+            console.log("     0=======   =======0                  ||")
+            console.log("              ||                     ====    ====")
+            console.log("              ||                          ||")
+            console.log("             //\\\\                         ||")
+            console.log("            //  \\\\                        || ")
+            console.log("           //    \\\\                  =============")
+            console.log("         <==      ==>                =============")
 
             alert("¡Es un empate! Ambos se aniquilaron.")
 
@@ -184,16 +189,16 @@ if (cond) {
 
     }
 
-    if(!game){
+    if (!game) {
         let tryagain = window.confirm("Desdes volver a jugar")
-    
-        if(tryagain){
+
+        if (tryagain) {
             location.reload();
-        }else{
+        } else {
             alert("Si cambias de opinión solo debes refrescar la página.")
         }
     }
-        
+
 
 } else {
     alert("La página se refrescará al presionar OK.");
