8efda9033f217596eefa4c3c785e1fb5c48d2573
Jonas <jonathan.avila.cruz@hotmail.com>
2022-08-31 08:16:10 -0400

    Actividad_2

--- script/actividad1.js
+++ script/actividad1.js
@@ -1,11 +1,12 @@
 // Esto lo hago pa divertirme ♪
+let imp = (msj) => console.log(msj);
 
-console.log("Modo de juego")
-console.log("Este es un juego en base a probabilidades")
-console.log("Robocito y tú tienen 20 de vida")
-console.log("Cada lanzamiento varía entre 0 y 10, que se irá restando a la vida del contrincante")
-console.log("Ganas si logras destruirlo antes que él a ti. Buena suerte!")
-console.log("")
+imp("Modo de juego")
+imp("Este es un juego en base a probabilidades")
+imp("Robocito y tú tienen 20 de vida")
+imp("Cada lanzamiento varía entre 0 y 10, que se irá restando a la vida del contrincante")
+imp("Ganas si logras destruirlo antes que él a ti. Buena suerte!")
+imp("")
 
 
 let cond = window.confirm("Debes abrir la consola con [F12] para ver las instrucciones y jugar. Si no las ves, presiona cancelar.");
@@ -33,129 +34,105 @@ if (cond) {
     let m4 = "Hoy la suerte no te sonríe"
     let m5 = "Definitivamente la suerte no te sonríe"
 
+    let caseFunct = (randomNumber) => {
+        switch (true) {
+            case (randomNumber > 9):
+                imp(m1);
+                break;
+            case (randomNumber > 4):
+                imp(m2);
+                break;
+            case (randomNumber > 2):
+                imp(m3);
+                break;
+            case (randomNumber > 0):
+                imp(m4);
+                break;
+            default:
+                imp(m5);
+                break;
+        }
+    }
 
     while (game) {
         console.clear();
-        if (p1 != undefined) {
-            console.log("Comments:")
-            if (!p1) {
-                console.log(name + " ha pasado su turno")
-            } else {
-                console.log(name + " ha sacado " + playerRandom1)
-                switch (true) {
-                    case (playerRandom1 > 9):
-                        console.log(m1);
-                        break;
-                    case (playerRandom1 > 4):
-                        console.log(m2);
-                        break;
-                    case (playerRandom1 > 2):
-                        console.log(m3);
-                        break;
-                    case (playerRandom1 > 0):
-                        console.log(m4);
-                        break;
-                    default:
-                        console.log(m5);
-                        break;
-                }
-                
-            }
-            console.log("")
-                console.log("Robocito ha sacado " + playerRandom2)
-            switch (true) {
-                case (playerRandom2 > 9):
-                    console.log(m1);
-                    break;
-                case (playerRandom2 > 4):
-                    console.log(m2);
-                    break;
-                case (playerRandom2 > 2):
-                    console.log(m3);
-                    break;
-                case (playerRandom2 > 0):
-                    console.log(m4);
-                    break;
-                default:
-                    console.log(m5);
-                    break;
-            }
 
-            console.log("_".repeat(60))
-            console.log(`${name} (${player1}) ` + character.repeat(player1))
-            console.log("_".repeat(60))
-
-            console.log("_".repeat(60))
-            console.log(`Robocito (${player2}) ` + character.repeat(player2))
-            console.log("_".repeat(60))
+        let lifeBar = (name, player1, player2) =>{
+            imp("_".repeat(60))
+            imp(`${name} (${player1}) ` + character.repeat(player1))
+            imp("_".repeat(60))
+            imp("_".repeat(60))
+            imp(`Robocito (${player2}) ` + character.repeat(player2))
+            imp("_".repeat(60))
         }
 
+        if (p1 != undefined) {
+            imp("Comments:")
+            if (!p1) {
+                imp(name + " ha pasado su turno")
+            } else {
+                imp(name + " ha sacado " + playerRandom1)
+                caseFunct(playerRandom1);
+            }
+            imp("")
+            imp("Robocito ha sacado " + playerRandom2)
+            caseFunct(playerRandom2);
+            lifeBar(name, player1, player2);
+        }
 
-        console.log("        __\\_/__")
-        console.log("        | @ @ |")
-        console.log("        | +++ | ")
-        console.log("  0=======   =======0 ")
-        console.log("          || ")
-        console.log("          || ")
-        console.log("         //\\\\")
-        console.log("        //  \\\\")
-        console.log("       //    \\\\")
-        console.log("     <==      ==>")
+        imp("        __\\_/__")
+        imp("        | @ @ |")
+        imp("        | +++ | ")
+        imp("  0=======   =======0 ")
+        imp("          || ")
+        imp("          || ")
+        imp("         //\\\\")
+        imp("        //  \\\\")
+        imp("       //    \\\\")
+        imp("     <==      ==>")
 
         p1 = window.confirm("Es tu turno ¿Deseas lanzar? Si presionas cancelar tu turno se saltará.")
-
         playerRandom1 = Math.floor(Math.random() * 10);
         playerRandom2 = Math.floor(Math.random() * 10);
 
         if (p1) {
             player2 = player2 - playerRandom1;
         }
-
         player1 = player1 - playerRandom2;
 
         if (player2 <= 0 && player1 > 0) {
             player2 = 0;
             game = false;
             console.clear();
-            console.log("_".repeat(60))
-            console.log(`${name} (${player1 < 0 ? 0 : player1}) ` + character.repeat(player1 < 0 ? 0 : player1))
-            console.log("_".repeat(60))
-            console.log("_".repeat(60))
-            console.log(`Robocito (${player2}) ` + character.repeat(player2))
-            console.log("_".repeat(60))
+            lifeBar(name, player1, player2);
 
-            console.log("        ~~    ~~")
-            console.log("        __\\_/__")
-            console.log("        | X X |")
-            console.log("        | ___ | ")
-            console.log("  0=======   =======0 ")
-            console.log("          || ")
-            console.log("          || ")
-            console.log("         //\\\\")
-            console.log("        //  \\\\")
-            console.log("       //    \\\\")
-            console.log("     <==      ==>")
+            imp("        ~~    ~~")
+            imp("        __\\_/__")
+            imp("        | X X |")
+            imp("        | ___ | ")
+            imp("  0=======   =======0 ")
+            imp("          || ")
+            imp("          || ")
+            imp("         //\\\\")
+            imp("        //  \\\\")
+            imp("       //    \\\\")
+            imp("     <==      ==>")
             alert("Derrotaste a Robocito ¡Felicitaciones!")
+
         } else if (player1 <= 0 && player2 > 0) {
             player1 = 0;
             game = false;
             console.clear();
-            console.log("_".repeat(60))
-            console.log(`${name} (${player1}) ` + character.repeat(player1))
-            console.log("_".repeat(60))
-            console.log("_".repeat(60))
-            console.log(`Robocito (${player2 < 0 ? 0 : player2}) ` + character.repeat(player2 < 0 ? 0 : player2))
-            console.log("_".repeat(60))
+            lifeBar(name, player1, player2);
 
-            console.log("        ||")
-            console.log("        ||")
-            console.log("   ====    ====")
-            console.log("        ||")
-            console.log("        || ")
-            console.log("        || ")
-            console.log("   =============")
-            console.log("   =============")
-
+            imp("        ||")
+            imp("        ||")
+            imp("   ====    ====")
+            imp("        ||")
+            imp("        || ")
+            imp("        || ")
+            imp("   =============")
+            imp("   =============")
             alert("Que lastima, no pudiste vencer a Robocito.")
 
         } else if (player1 <= 0 && player2 <= 0) {
@@ -163,30 +140,22 @@ if (cond) {
             player2 = 0;
             game = false;
             console.clear();
-            console.log("_".repeat(60))
-            console.log(`${name} (${player1}) ` + character.repeat(player1))
-            console.log("_".repeat(60))
-            console.log("_".repeat(60))
-            console.log(`Robocito (${player2}) ` + character.repeat(player2))
-            console.log("_".repeat(60))
+            lifeBar(name, player1, player2);
 
-
-            console.log("           ~~    ~~")
-            console.log("           __\\_/__")
-            console.log("           | X X |")
-            console.log("           | ___ |                        ||")
-            console.log("     0=======   =======0                  ||")
-            console.log("              ||                     ====    ====")
-            console.log("              ||                          ||")
-            console.log("             //\\\\                         ||")
-            console.log("            //  \\\\                        || ")
-            console.log("           //    \\\\                  =============")
-            console.log("         <==      ==>                =============")
-
+            imp("           ~~    ~~")
+            imp("           __\\_/__")
+            imp("           | X X |")
+            imp("           | ___ |                        ||")
+            imp("     0=======   =======0                  ||")
+            imp("              ||                     ====    ====")
+            imp("              ||                          ||")
+            imp("             //\\\\                         ||")
+            imp("            //  \\\\                        || ")
+            imp("           //    \\\\                  =============")
+            imp("         <==      ==>                =============")
             alert("¡Es un empate! Ambos se aniquilaron.")
 
         }
-
     }
 
     if (!game) {
@@ -198,7 +167,6 @@ if (cond) {
             alert("Si cambias de opinión solo debes refrescar la página.")
         }
     }
-
 
 } else {
     alert("La página se refrescará al presionar OK.");
--- script/ejercicio_en_clases.js
+++ script/ejercicio_en_clases.js
@@ -15,31 +15,50 @@
 //     console.log(`El contador es: ${i} `)
 // }
 
-let n1 = Number(prompt("Ingrese un numero"))
+// let n1 = Number(prompt("Ingrese un numero"))
 
-for(let i = 1; i<= 10; i++){
-    let result = i * n1;
-    console.log(`${n1} x ${i} = ${result}`);
-}
+// for(let i = 1; i<= 10; i++){
+//     let result = i * n1;
+//     console.log(`${n1} x ${i} = ${result}`);
+// }
 
-console.log("==========================")
+// console.log("==========================")
 
-for(let i = 1; i<= 10; i++){
-    let result = i * n1;
+// for(let i = 1; i<= 10; i++){
+//     let result = i * n1;
 
-    if(i === 6){
-        break;
-    }
-    console.log(`${n1} x ${i} = ${result}`);
-}
+//     if(i === 6){
+//         break;
+//     }
+//     console.log(`${n1} x ${i} = ${result}`);
+// }
 
-console.log("==========================")
+// console.log("==========================")
 
-for(let i = 1; i<= 10; i++){
-    let result = i * n1;
+// for(let i = 1; i<= 10; i++){
+//     let result = i * n1;
 
-    if(i === 6){
-        continue;
-    }
-    console.log(`${n1} x ${i} = ${result}`);
-}
+//     if(i === 6){
+//         continue;
+//     }
+//     console.log(`${n1} x ${i} = ${result}`);
+// }
+
+
+// // Funciones
+// function sumar(primerNumero, segundoNumero) {
+//     let resultado = primerNumero + segundoNumero;
+//    }
+
+// //Anonimas
+// const suma = function (a, b) { return a + b }
+// const resta = function (a, b) { return a - b }
+// console.log( suma(15,20) )
+// console.log( resta(15,5) )
+
+// //Fat arrow funtion
+// const suma1 = (a, b) => { return a + b }
+// //Si es una función de una sola línea con retorno podemos evitar escribir el cuerpo.
+// const resta1 = (a, b) => a - b ;
+// console.log( suma(15,20) )
+// console.log( resta(20,5) )
